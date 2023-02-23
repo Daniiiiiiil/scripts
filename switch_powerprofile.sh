a2b36b2f05c4af084e7d4b404977aad4b9148f04
Daniyl <daniylkogan@gmail.com>
2023-02-24 00:57:47 +0200

    Refactored: switch_powerprofile.sh

--- switch_powerprofile.sh
+++ switch_powerprofile.sh
@@ -2,19 +2,18 @@
 
 # Dependencies: libnotify, power-profiles-daemon
 
-current_powerprofile=$(powerprofilesctl | grep \*)
-current_powerprofile=${current_powerprofile:2:-1}
+current_powerprofile=$(powerprofilesctl get)
 
 if [ "$current_powerprofile" == "power-saver" ]; then
-    powerprofilesctl set balanced
+    powerprofilesctl set balanced && \
     notify-send "Balanced Mode Enabled" -e -i power-profile-balanced
 
 elif [ "$current_powerprofile" == "balanced" ]; then
-    powerprofilesctl set performance
+    powerprofilesctl set performance && \
     notify-send "Performance Mode Enabled" -e -i power-profile-performance
     
 else
-    powerprofilesctl set power-saver
+    powerprofilesctl set power-saver && \
     notify-send "Power-Saver Mode Enabled" -e -i power-profile-power-saver
     
 fi
